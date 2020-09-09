da69d481885d21fcdc06615deb943c0f5cebf532
Everton Araujo <everton.aaraujo@outlook.com>
2020-09-09 10:27:01 -0300

    Created Users and updated appointment migration

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn, UpdateDateColumn, CreateDateColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, UpdateDateColumn, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -6,7 +8,11 @@ class Appointment {
     id: string;
 
     @Column() //vazio assume um varchar
-    provider: string;
+    provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'provider_id' })
+    provider: User;
 
     @Column('timestamp with time zone')
     date: Date;
