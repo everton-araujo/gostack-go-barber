7bf095d026b5543c9676c6613846250979c833f0
Everton Araujo <everton.aaraujo@outlook.com>
2020-09-11 09:28:05 -0300

    Upload files

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -23,6 +27,13 @@ usersRouter.post('/', async (request, response) => {
     } catch (err) {
         return response.status(400).json({ error: err.message });
     }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+    console.log(request.file);
+    console.log(request.file.filename.split('-')[1]);
+
+    return response.json({ ok: true });
 });
 
 export default usersRouter;
