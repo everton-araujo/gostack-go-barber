c0b42c339eb81c6576b1c39f717c90d98f67a17f
Everton Araujo <everton.aaraujo@outlook.com>
2020-09-11 10:23:09 -0300

    Typo adjustment

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-exceptionimport express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
 import routes from './routes';
