31a30f2919c89224cf09975674e87a80c9e7a0a7
Everton Araujo <everton.aaraujo@outlook.com>
2020-10-29 07:22:02 -0300

    Cache config with redis

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
     provider_id: string;
@@ -14,16 +15,26 @@ interface IRequest {
 class ListProviderAppointmentsService {
     constructor(
         @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository
+        private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider
     ) { }
 
     public async execute({ provider_id, day, month, year }: IRequest): Promise<Appointment[]> {
+        const cacheData = await this.cacheProvider.recover('sa');
+
+        console.log(cacheData);
+
+
         const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
             provider_id,
             day,
             month,
             year,
         });
+
+        // await this.cacheProvider.save('sa', 'asd');
 
         return appointments;
     }
